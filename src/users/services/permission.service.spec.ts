49250a36d2b2068ad5d401c202e710d3473cc784
Gabriellji <gabriellji@yahoo.com>
2021-05-17 19:48:12 +0300

    feat/tests for get permission by ID permission case

--- src/users/services/permission.service.spec.ts
+++ src/users/services/permission.service.spec.ts
@@ -1,18 +1,72 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Connection, Repository } from 'typeorm';
+import { permissionRepository } from '../repositories/permission.repository';
 import { PermissionService } from './permission.service';
+
+type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
+const createMockRepository = <T = any>(): MockRepository<T> => ({
+  find: jest.fn(),
+  findOne: jest.fn(),
+  create: jest.fn(),
+  save: jest.fn(),
+  update: jest.fn(),
+  delete: jest.fn(),
+});
 
 describe('PermissionService', () => {
   let service: PermissionService;
+  let rolesRepository: MockRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PermissionService],
+      providers: [
+        PermissionService,
+        { provide: Connection, useValue: {} },
+        {
+          provide: getRepositoryToken(permissionRepository),
+          useValue: createMockRepository(),
+        },
+      ],
     }).compile();
 
     service = module.get<PermissionService>(PermissionService);
+    rolesRepository = module.get<MockRepository>(
+      getRepositoryToken(permissionRepository),
+    );
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('find User by ID', () => {
+    describe('when user with ID exists', () => {
+      it('should return the user object', async () => {
+        const permissionId = 1;
+        const expectedPermission = {};
+
+        rolesRepository.findOne.mockReturnValue(expectedPermission);
+        const user = await service.getPermissionById(permissionId);
+        expect(user).toEqual(expectedPermission);
+      });
+    });
+
+    describe('otherwise', () => {
+      it('should throw the "NotFoundException"', async () => {
+        const permissionId = 1;
+        rolesRepository.findOne.mockReturnValue(undefined);
+
+        try {
+          await service.getPermissionById(permissionId);
+        } catch (err) {
+          expect(err).toBeInstanceOf(NotFoundException);
+          expect(err.message).toEqual(
+            `Permission with ID "${permissionId}" not found`,
+          );
+        }
+      });
+    });
   });
 });
