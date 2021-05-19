ba7addbcfda69bf5a62d37b2a7e18d6a12494837
Gabriellji <gabriellji@yahoo.com>
2021-05-19 19:00:41 +0300

    feat/implement test for get all users case in user controller

--- src/users/controllers/users.controller.spec.ts
+++ src/users/controllers/users.controller.spec.ts
@@ -3,13 +3,34 @@ import { usersRepository } from '../repositories/users.repository';
 import { UsersService } from '../services/users.service';
 import { UsersController } from './users.controller';
 
+const testUser = {
+  id: 1,
+  firstName: 'Zheniya',
+  lastName: 'Best Dev Ever',
+  password: 'bestOfTheBest',
+  email: 'test@test.com',
+  role: ['user'],
+};
+
 describe('UsersController', () => {
   let controller: UsersController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [UsersService, usersRepository],
+      providers: [
+        usersRepository,
+        {
+          provide: UsersService,
+          useValue: {
+            getAll: jest.fn(() => [testUser]),
+            getUserById: jest.fn(() => testUser),
+            createUser: jest.fn(() => testUser),
+            updateUser: jest.fn(() => testUser),
+            deleteUser: jest.fn(() => testUser.id),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
@@ -17,5 +38,12 @@ describe('UsersController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('get all users', () => {
+    it('should get the list of users', async () => {
+      const users = await controller.getAll();
+      expect(users).toEqual([testUser]);
+    });
   });
 });
