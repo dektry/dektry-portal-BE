c23ea0a6ef184410a719374f20d87e881e9737a2
Eugen Bezrukov <54332269+dmitriy-homza@users.noreply.github.com>
2021-07-17 12:59:56 +0300

    feat: prepare for deploy

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,13 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
-    origin: process.env.API_URL,
+    origin: [
+      'https://dektry-fe-build.herokuapp.com',
+      'https://herokuapp.com',
+      'https://web.app',
+      'http://localhost:3001',
+      'http://localhost:3000',
+    ],
     credentials: true,
   });
   await app.listen(process.env.PORT || 5000);
