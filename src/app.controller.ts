ad3c081d770916044ec57dc5f906af530f7cfc3c
Eugen Bezrukov <54332269+dmitriy-homza@users.noreply.github.com>
2021-07-17 13:55:28 +0300

    fix: homepage

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,7 +3,7 @@ import { Controller, Get, Res } from '@nestjs/common';
 @Controller()
 export class AppController {
   @Get()
-  homePage(@Res() res) {
-    return res.redirect();
+  homePage(): string {
+    return "Hello, it seem's like API is ok!";
   }
 }
