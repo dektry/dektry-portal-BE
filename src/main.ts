487d898673de07a9854b44dd866a15e8a8dfdd3f
Eugen Bezrukov <54332269+dmitriy-homza@users.noreply.github.com>
2021-07-17 11:34:03 +0300

    fix: auth method from cookie to bearer token

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,16 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import * as cookieParser from 'cookie-parser';
 import dotEnv = require('dotenv');
 
 async function bootstrap() {
   await dotEnv.config();
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.use(cookieParser());
   app.enableCors({
     origin: process.env.API_URL,
     credentials: true,
   });
-  await app.listen(process.env.API_PORT);
+  await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
