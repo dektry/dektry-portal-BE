f879fb018f92048ca253fcbaf41eb8b1d46b6ead
NastassiaTsymanovich <nastassia.tsymanovich@dektry.com>
2021-08-09 15:16:24 +0300

    fix: add projects migration

--- src/migration/1626977032418-init.ts
+++ src/migration/1626977032418-init.ts
@@ -11,8 +11,6 @@ export class init1626977032418 implements MigrationInterface {
         await queryRunner.query(`CREATE TABLE "users" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "firstName" character varying(20) NOT NULL, "lastName" character varying(20) NOT NULL, "email" character varying(40) NOT NULL, "password" character varying(255) NOT NULL, "avatarFileName" character varying(255) NOT NULL DEFAULT 'default_admin.png', "isActive" boolean NOT NULL DEFAULT true, "birthday" TIMESTAMP WITH TIME ZONE NOT NULL, "role" uuid, CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3" UNIQUE ("email"), CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TABLE "careers" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "salary" integer NOT NULL, "from" TIMESTAMP WITH TIME ZONE NOT NULL, "to" TIMESTAMP WITH TIME ZONE, "userId" uuid, "positionId" uuid, CONSTRAINT "PK_febfc45dc83d58090d3122fde3d" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TABLE "role_permissions" ("rolesId" uuid NOT NULL, "permissionsId" uuid NOT NULL, CONSTRAINT "PK_7931614007a93423204b4b73240" PRIMARY KEY ("rolesId", "permissionsId"))`);
-        await queryRunner.query(`CREATE TABLE "projects" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying(255) NOT NULL, "managers" uuid array, "users" uuid array, "isArchive" boolean, CONSTRAINT PK_projects PRIMARY KEY (id))`);
-        await queryRunner.query(`CREATE TABLE "projects_history" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "from" TIMESTAMP WITH TIME ZONE NOT NULL, "to" TIMESTAMP WITH TIME ZONE, "userId" uuid, "projectId" uuid, CONSTRAINT "PK_project_history" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE INDEX "IDX_0cb93c5877d37e954e2aa59e52" ON "role_permissions" ("rolesId") `);
         await queryRunner.query(`CREATE INDEX "IDX_d422dabc78ff74a8dab6583da0" ON "role_permissions" ("permissionsId") `);
         await queryRunner.query(`ALTER TABLE "positions" ADD CONSTRAINT "FK_8a06d0e75390aec8c429ca2dbf2" FOREIGN KEY ("groupId") REFERENCES "positionGroup"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
@@ -21,8 +19,6 @@ export class init1626977032418 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE "careers" ADD CONSTRAINT "FK_add2cbbf1edb12010b16437822e" FOREIGN KEY ("positionId") REFERENCES "positions"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE "role_permissions" ADD CONSTRAINT "FK_0cb93c5877d37e954e2aa59e52c" FOREIGN KEY ("rolesId") REFERENCES "roles"("id") ON DELETE CASCADE ON UPDATE CASCADE`);
         await queryRunner.query(`ALTER TABLE "role_permissions" ADD CONSTRAINT "FK_d422dabc78ff74a8dab6583da02" FOREIGN KEY ("permissionsId") REFERENCES "permissions"("id") ON DELETE CASCADE ON UPDATE CASCADE`);
-        await queryRunner.query(`ALTER TABLE "projects_history" ADD CONSTRAINT "FK_projects_history_user_id" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
-        await queryRunner.query(`ALTER TABLE "projects_history" ADD CONSTRAINT "FK_projects_history_project_id" FOREIGN KEY ("projectId") REFERENCES "projects"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
@@ -32,8 +28,6 @@ export class init1626977032418 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE "careers" DROP CONSTRAINT "FK_2c1cbd799cb89256137fd6e328e"`);
         await queryRunner.query(`ALTER TABLE "users" DROP CONSTRAINT "FK_ace513fa30d485cfd25c11a9e4a"`);
         await queryRunner.query(`ALTER TABLE "positions" DROP CONSTRAINT "FK_8a06d0e75390aec8c429ca2dbf2"`);
-        await queryRunner.query(`ALTER TABLE "projects_history" DROP CONSTRAINT "FK_projects_history_user_id"`);
-        await queryRunner.query(`ALTER TABLE "projects_history" DROP CONSTRAINT "FK_projects_history_project_id"`);
         await queryRunner.query(`DROP INDEX "IDX_d422dabc78ff74a8dab6583da0"`);
         await queryRunner.query(`DROP INDEX "IDX_0cb93c5877d37e954e2aa59e52"`);
         await queryRunner.query(`DROP TABLE "role_permissions"`);
@@ -43,8 +37,6 @@ export class init1626977032418 implements MigrationInterface {
         await queryRunner.query(`DROP TABLE "permissions"`);
         await queryRunner.query(`DROP TABLE "positions"`);
         await queryRunner.query(`DROP TABLE "positionGroup"`);
-        await queryRunner.query(`DROP TABLE "projects"`);
-        await queryRunner.query(`DROP TABLE "projects_history"`);
     }
 
 }
