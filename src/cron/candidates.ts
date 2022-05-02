2409cc79b3ea8cd50e58aa09af4d0e35154c4abe
samartsevnik <hukuta08@gmail.com>
2022-05-02 08:40:04 -0300

    return candidatesCron time working

--- src/cron/candidates.ts
+++ src/cron/candidates.ts
@@ -1,7 +1,7 @@
 import { CronJob } from 'cron';
 import { getCandidates } from '../candidates/services/candidates';
 
-export const candidatesCron = new CronJob('*/5 * * * *', async () => {
+export const candidatesCron = new CronJob('*/50 * * * *', async () => {
   console.log('fetching data by cron');
   try {
     await getCandidates();
