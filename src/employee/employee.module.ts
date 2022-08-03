f9681027d06ace5f9292685464c2c78703635c78
Shkuratov Artem <shuratov2001@mail.ru>
2022-08-03 16:13:02 +0300

    added employee interview tables

--- src/employee/employee.module.ts
+++ src/employee/employee.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
-import { EmployeeController } from './controllers/employee.controller';
-import { EmployeeService } from './services/employee.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { EmployeeController } from './controllers/employee.controller';
+
+import { EmployeeService } from './services/employee.service';
+
 import { employeeRepository } from './repositories/employee.repository';
+import { employeeInterviewRepository } from './repositories/interview.repository';
+import { employeeSkillToInterviewRepository } from './repositories/skillToInterview.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([employeeRepository])],
+  imports: [
+    TypeOrmModule.forFeature([
+      employeeRepository,
+      employeeInterviewRepository,
+      employeeSkillToInterviewRepository,
+    ]),
+  ],
   controllers: [EmployeeController],
   providers: [EmployeeService],
 })
