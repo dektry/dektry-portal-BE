920aa98be1373ad5d8eb01537fe1c142a313a402
takoi <dzianis.khomich@dektry.com>
2022-09-19 15:22:22 +0200

    adds try catch and logger

--- src/cv-generation/services/cv-generation.service.ts
+++ src/cv-generation/services/cv-generation.service.ts
@@ -1,4 +1,4 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, Logger } from '@nestjs/common';
 import { Readable } from 'typeorm/platform/PlatformTools';
 import * as fs from 'fs';
 import * as path from 'path';
@@ -17,8 +17,9 @@ export class CVGenerationService {
         `./src/cv-generation/templates/${name}.html`,
       );
       return await readFile(templatePath, 'utf8');
-    } catch (err) {
-      console.log('Could not load html template');
+    } catch (error) {
+      Logger.error(error);
+
       throw new HttpException(templateNotFound, HttpStatus.NOT_FOUND);
     }
   }
@@ -28,20 +29,26 @@ export class CVGenerationService {
       throw new HttpException(noTemplate, HttpStatus.BAD_REQUEST);
     }
 
-    const browser = await puppeteer.launch({
-      // might be useful for debugging
-      // headless: false,
-    });
-    const page = await browser.newPage();
-    // the width and height are the same as the width and height in the template
-    await page.setViewport({ width: 595, height: 842 });
+    try {
+      const browser = await puppeteer.launch({
+        // might be useful for debugging
+        // headless: false,
+      });
+      const page = await browser.newPage();
+      // the width and height are the same as the width and height in the template
+      await page.setViewport({ width: 595, height: 842 });
 
-    await page.setContent(template);
+      await page.setContent(template);
 
-    const pdf = await page.pdf();
+      const pdf = await page.pdf();
 
-    await browser.close();
+      await browser.close();
 
-    return Readable.from(pdf);
+      return Readable.from(pdf);
+    } catch (error) {
+      Logger.error(error);
+
+      throw new HttpException('', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 }
