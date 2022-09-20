de98ee3396bf59b9afd7d908988c25b97b488cf4
DenisHomichDektry <73819600+DenisHomichDektry@users.noreply.github.com>
2022-09-20 13:52:30 +0200

    feat/CVGE-256-generate-pdf (#44)
    * basic structure of cv-generation
    
    * adds cv-generation service
    
    * adds HttpException instead manual response status assigning
    
    * adds generatePdf service and controller
    
    * return generated pdf as stream
    
    * removes header from bad request
    
    * adds try catch and logger
    
    * restricts javascript in pupeteer
    
    * adds process.cwd for path, updates postman collection, adds validation for cv generation req, removes excessive try catch
    
    * proper usage of path.join
    
    Co-authored-by: takoi <dzianis.khomich@dektry.com>

--- src/cv-generation/services/cv-generation.service.ts
+++ src/cv-generation/services/cv-generation.service.ts
@@ -1,7 +1,9 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, Logger } from '@nestjs/common';
+import { Readable } from 'typeorm/platform/PlatformTools';
 import * as fs from 'fs';
 import * as path from 'path';
 import { promisify } from 'util';
+import puppeteer from 'puppeteer';
 
 import { templateNotFound } from '../constants/messages';
 
@@ -11,13 +13,39 @@ const readFile = promisify(fs.readFile);
 export class CVGenerationService {
   async getTemplate(name: string) {
     try {
-      const templatePath = path.resolve(
-        `./src/cv-generation/templates/${name}.html`,
+      const templatePath = path.join(
+        process.cwd(),
+        'src',
+        'cv-generation',
+        'templates',
+        `${name}.html`,
       );
       return await readFile(templatePath, 'utf8');
-    } catch (err) {
-      console.log('Could not load html template');
+    } catch (error) {
+      Logger.error(error);
+
       throw new HttpException(templateNotFound, HttpStatus.NOT_FOUND);
     }
+  }
+
+  async generatePdf(template: string) {
+    const browser = await puppeteer.launch({
+      // might be useful for debugging
+      // headless: false,
+    });
+    const page = await browser.newPage();
+    // the width and height are the same as the width and height in the template
+    await page.setViewport({ width: 595, height: 842 });
+
+    // somebody will try to inject script (I tried)
+    await page.setJavaScriptEnabled(false);
+
+    await page.setContent(template);
+
+    const pdf = await page.pdf({ timeout: 10000 });
+
+    await browser.close();
+
+    return Readable.from(pdf);
   }
 }
