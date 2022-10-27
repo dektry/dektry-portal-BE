1de2190b0f9fda9b509d15e438c78a34b156f175
Anton Bochkarev <a.n.bochkarev1991@gmail.com>
2022-10-27 11:05:03 +0300

    level migration fix

--- src/migration/1666694513234-languageTable.ts
+++ src/migration/1666694513234-languageTable.ts
@@ -4,7 +4,7 @@ export class languageTable1666694513234 implements MigrationInterface {
     name = 'languageTable1666694513234'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DELETE * FROM "language"`);
+        await queryRunner.query(`DELETE FROM "language"`);
         await queryRunner.query(`ALTER TABLE "language" ADD "value" character varying(255) NOT NULL`);
         await queryRunner.query(`ALTER TABLE "language" ADD "level" character varying(5) NOT NULL`);
     }
