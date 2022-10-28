9016681334a6cbacccfb3390b5971ad477046e87
Anton Bochkarev <a.n.bochkarev1991@gmail.com>
2022-10-28 10:50:50 +0300

    migration fix

--- src/migration/1666863316538-langFieldsNullable.ts
+++ src/migration/1666863316538-langFieldsNullable.ts
@@ -4,27 +4,12 @@ export class langFieldsNullable1666863316538 implements MigrationInterface {
   name = 'langFieldsNullable1666863316538';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE "language" DROP COLUMN "value"`);
     await queryRunner.query(
       `ALTER TABLE "language" ADD "value" character varying(255)`,
     );
     await queryRunner.query(`ALTER TABLE "language" ADD "employee_id" uuid`);
     await queryRunner.query(
-      `ALTER TABLE "language" ADD "code" character varying(40)`,
-    );
-    await queryRunner.query(`ALTER TABLE "language" DROP COLUMN "level"`);
-    await queryRunner.query(
       `ALTER TABLE "language" ADD "level" character varying(5)`,
-    );
-    await queryRunner.query(`ALTER TABLE "language" DROP COLUMN "level"`);
-    await queryRunner.query(
-      `ALTER TABLE "language" ADD "level" character varying(40)`,
-    );
-    await queryRunner.query(
-      `CREATE INDEX "IDX_465b3173cdddf0ac2d3fe73a33" ON "language" ("code") `,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "language" ADD CONSTRAINT "UQ_7032779f9862923a1f4f315b726" UNIQUE ("level", "code")`,
     );
     await queryRunner.query(
       `ALTER TABLE "language" ADD CONSTRAINT "FK_86e3d2644703d9f89cc3db67aa9" FOREIGN KEY ("employee_id") REFERENCES "employee"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
@@ -32,29 +17,9 @@ export class langFieldsNullable1666863316538 implements MigrationInterface {
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(
-      `ALTER TABLE "language" DROP CONSTRAINT "FK_86e3d2644703d9f89cc3db67aa9"`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "language" DROP CONSTRAINT "UQ_7032779f9862923a1f4f315b726"`,
-    );
-    await queryRunner.query(
-      `DROP INDEX "public"."IDX_465b3173cdddf0ac2d3fe73a33"`,
-    );
     await queryRunner.query(`ALTER TABLE "language" DROP COLUMN "level"`);
-    await queryRunner.query(
-      `ALTER TABLE "language" ADD "level" character varying(5)`,
-    );
-    await queryRunner.query(`ALTER TABLE "language" DROP COLUMN "level"`);
-    await queryRunner.query(
-      `ALTER TABLE "language" ADD "level" character varying(40) NOT NULL`,
-    );
-    await queryRunner.query(`ALTER TABLE "language" DROP COLUMN "code"`);
     await queryRunner.query(`ALTER TABLE "language" DROP COLUMN "employee_id"`);
     await queryRunner.query(`ALTER TABLE "language" DROP COLUMN "value"`);
-    await queryRunner.query(
-      `ALTER TABLE "language" ADD "value" character varying(255) NOT NULL`,
-    );
     await queryRunner.query(`ALTER TABLE "language" ADD "employee_id" uuid`);
     await queryRunner.query(
       `ALTER TABLE "language" ADD CONSTRAINT "FK_86e3d2644703d9f89cc3db67aa9" FOREIGN KEY ("employee_id") REFERENCES "employee"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
