5e02500b1e1d701fd5c43708d349fde2450177d9
Anton Bochkarev <a.n.bochkarev1991@gmail.com>
2022-10-28 12:07:54 +0300

    migration fix

--- src/migration/1666863316538-langFieldsNullable.ts
+++ src/migration/1666863316538-langFieldsNullable.ts
@@ -5,12 +5,13 @@ export class langFieldsNullable1666863316538 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `CREATE TABLE ("id") uuid NOT NULL DEFAULT uuid_generate_v4(), "value" character varying(255) NOT NULL, "level" character varying(5) NOT NULL, CONSTRAINT "FK_86e3d2644703d9f89cc3db67aa9" FOREIGN KEY ("employee_id") REFERENCES "employee"("id") ON DELETE CASCADE ON UPDATE NO ACTION)`,
+      `CREATE TABLE ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "value" character varying(255) NOT NULL, "level" character varying(5) NOT NULL, CONSTRAINT "FK_86e3d2644703d9f89cc3db67aa9" FOREIGN KEY ("employee_id") REFERENCES "employee"("id") ON DELETE CASCADE ON UPDATE NO ACTION)`,
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`ALTER TABLE "language" DROP COLUMN "level"`);
+
     await queryRunner.query(`ALTER TABLE "language" DROP COLUMN "employee_id"`);
     await queryRunner.query(`ALTER TABLE "language" DROP COLUMN "value"`);
     await queryRunner.query(`ALTER TABLE "language" DROP COLUMN "employee_id"`);
