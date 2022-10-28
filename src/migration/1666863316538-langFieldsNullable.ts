33b534b294e283645be0bf4004816ef45acf8bd5
Anton Bochkarev <a.n.bochkarev1991@gmail.com>
2022-10-28 11:34:02 +0300

    migration fix

--- src/migration/1666863316538-langFieldsNullable.ts
+++ src/migration/1666863316538-langFieldsNullable.ts
@@ -4,15 +4,10 @@ export class langFieldsNullable1666863316538 implements MigrationInterface {
   name = 'langFieldsNullable1666863316538';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DELETE FROM "candidates"`);
+    await queryRunner.query(`DROP TABLE "language"`);
     await queryRunner.query(
-      `ALTER TABLE "language" ADD "value" character varying(255)`,
-    );
-    await queryRunner.query(`ALTER TABLE "language" ADD "employee_id" uuid`);
-    await queryRunner.query(
-      `ALTER TABLE "language" ADD COLUMN "level" character varying(5)`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "language" ADD CONSTRAINT "FK_86e3d2644703d9f89cc3db67aa9" FOREIGN KEY ("employee_id") REFERENCES "employee"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
+      `CREATE TABLE ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "value" character varying(255) NOT NULL, "level" character varying(5) NOT NULL, CONSTRAINT "FK_86e3d2644703d9f89cc3db67aa9" FOREIGN KEY ("employee_id") REFERENCES "employee"("id") ON DELETE CASCADE ON UPDATE NO ACTION)`,
     );
   }
 
