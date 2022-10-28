47ce452265ebae964c0eca66b67c1181bffadb2d
Anton Bochkarev <a.n.bochkarev1991@gmail.com>
2022-10-28 11:36:52 +0300

    migration fix

--- src/migration/1666863316538-langFieldsNullable.ts
+++ src/migration/1666863316538-langFieldsNullable.ts
@@ -4,7 +4,7 @@ export class langFieldsNullable1666863316538 implements MigrationInterface {
   name = 'langFieldsNullable1666863316538';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`DELETE FROM "candidates"`);
+    await queryRunner.query(`DELETE FROM "candidate"`);
     await queryRunner.query(`DROP TABLE "language"`);
     await queryRunner.query(
       `CREATE TABLE ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "value" character varying(255) NOT NULL, "level" character varying(5) NOT NULL, CONSTRAINT "FK_86e3d2644703d9f89cc3db67aa9" FOREIGN KEY ("employee_id") REFERENCES "employee"("id") ON DELETE CASCADE ON UPDATE NO ACTION)`,
