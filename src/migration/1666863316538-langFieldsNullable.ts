4f1ecefcea8225710a6ba6d956b1538f8b2f1272
Anton Bochkarev <a.n.bochkarev1991@gmail.com>
2022-10-28 10:43:10 +0300

    migration fix

--- src/migration/1666863316538-langFieldsNullable.ts
+++ src/migration/1666863316538-langFieldsNullable.ts
@@ -4,9 +4,6 @@ export class langFieldsNullable1666863316538 implements MigrationInterface {
   name = 'langFieldsNullable1666863316538';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(
-      `ALTER TABLE "language" DROP CONSTRAINT "FK_86e3d2644703d9f89cc3db67aa9"`,
-    );
     await queryRunner.query(`ALTER TABLE "language" DROP COLUMN "employee_id"`);
     await queryRunner.query(`ALTER TABLE "language" DROP COLUMN "value"`);
     await queryRunner.query(
