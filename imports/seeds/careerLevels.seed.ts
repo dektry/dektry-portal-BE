94c3c1181cbd113f31445beaad8ab70765626328
Anton Bochkarev <a.n.bochkarev1991@gmail.com>
2022-11-01 14:52:17 +0300

    new seed info for positions, levels, skill groups and skills

--- imports/seeds/careerLevels.seed.ts
+++ imports/seeds/careerLevels.seed.ts
@@ -1,74 +1,14 @@
 export const careerLevelsSeed = [
   {
-    name: 'Trainee',
-    positions: [
-      'Front-end developer',
-      'Back-end developer',
-      'Full stack developer',
-      'Swift developer',
-    ],
-  },
-  {
-    name: 'Junior',
-    positions: [
-      'Front-end developer',
-      'Back-end developer',
-      'Full stack developer',
-      'Swift developer',
-    ],
-  },
-  {
-    name: 'Regular (Junior+)',
-    positions: [
-      'Front-end developer',
-      'Back-end developer',
-      'Full stack developer',
-      'Swift developer',
-    ],
-  },
-  {
     name: 'Middle',
-    positions: [
-      'Front-end developer',
-      'Back-end developer',
-      'Full stack developer',
-      'Swift developer',
-    ],
+    positions: ['Full stack developer'],
   },
   {
     name: 'Middle+',
-    positions: [
-      'Front-end developer',
-      'Back-end developer',
-      'Full stack developer',
-      'Swift developer',
-    ],
+    positions: ['Full stack developer'],
   },
   {
     name: 'Senior',
-    positions: [
-      'Front-end developer',
-      'Back-end developer',
-      'Full stack developer',
-      'Swift developer',
-    ],
-  },
-  {
-    name: 'Lead',
-    positions: [
-      'Front-end developer',
-      'Back-end developer',
-      'Full stack developer',
-      'Swift developer',
-    ],
-  },
-  {
-    name: 'Deleted level',
-    positions: [
-      'Front-end developer',
-      'Back-end developer',
-      'Full stack developer',
-      'Swift developer',
-    ],
+    positions: ['Full stack developer'],
   },
 ];
